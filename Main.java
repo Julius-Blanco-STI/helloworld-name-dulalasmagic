88a2f8c41fcb3b627b636156c938dbdb06944bad
dulalasmagic <58576151+dulalasmagic@users.noreply.github.com>
2019-12-06 08:43:09 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+package main;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    String name = "Earvin John"; 
+    System.out.println("Hello " + name);
   }
 
 }
