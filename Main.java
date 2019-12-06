a71fa157b3b752ff35e8be47128406c0ef116317
dulalasmagic <58576151+dulalasmagic@users.noreply.github.com>
2019-12-06 09:07:20 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
 package main;
+import java.util.*;
 public class Main {
 
   public static void main(String[] args) {
-    String name = "Earvin John"; 
-    System.out.println("Hello " + name);
+    Scanner scn = new Scanner(System.in);
+    
+    System.out.println("what's your name? ");
+        String name = scn.nextLine();
+    System.out.println("How old are you? ");
+        int age = scn.nextInt();
+    System.out.println("What's your address? ");
+        String home = scn.nextLine();
+               home = scn.nextLine();
+    
+    
+        
+    System.out.println("");    
+    System.out.println("");
+    
+    System.out.println("Hi "+ name +("!")); 
+ 
+    
   }
 
 }
