402114d39460cc848940df8c5d078ccdea424aa6
dulalasmagic <58576151+dulalasmagic@users.noreply.github.com>
2019-12-09 12:29:25 -1200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,20 @@
 package main;
 import java.util.*;
 public class Main {
+  
+public static void main(String[] args) {
+Scanner scn = new Scanner(System.in);
 
-  public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
-
-    System.out.println("what's your name? ");
-        String name = scn.nextLine();
-    System.out.println("How old are you? ");
-        int age = scn.nextInt();
-    System.out.println("What's your address? ");
-        String home = scn.nextLine();
-               home = scn.nextLine();
-
-
-
-    System.out.println("");    
-    System.out.println("");
-
-    System.out.println("Hi "+ name +("!")); 
-
-
+System.out.println("what's your name? ");
+  String name = scn.nextLine();
+System.out.println("How old are you? ");
+  int age = scn.nextInt();
+System.out.println("What's your address? ");
+  String home = scn.nextLine();
+         home = scn.nextLine();
+  
+System.out.println("");    
+System.out.println("");
+System.out.println("Hi "+ name +("!")); 
   }
-
 } 
